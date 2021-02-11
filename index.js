6a828a747b38e60dda579daf6f8abcbc286366a9
Rose Ralak <ralakrose@gmail.com>
2021-02-11 11:14:08 +0300

    css here and there

---
+++ index.js
@@ -0,0 +1,12 @@
+let username = document.getElementById("username");
+let registration = document.getElementById("registration");
+let userid = document.getElementById("userid");
+let mobilenumber = document.getElementById("mobilenumber");
+let email = document.getElementById("email");
+let file = document.getElementById("file");
+let address = document.getElementById("address");
+let country = document.getElementById("country");
+let zip = document.getElementById("zip");
+
+let fsex = document.getElementById("fsex");
+let msex = document.getElementById("msex");
